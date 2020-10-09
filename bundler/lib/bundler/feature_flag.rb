shyamalschandra/bundler

Make relativeness of config a feature flag

lib/bundler/feature_flag.rb
@@ -25,6 +25,15 @@ module Bundler
     end
     private_class_method :settings_method
 
+    def self.env_flag(name, &default)
+      define_method(:"#{name}?") do
+        value = ENV["BUNDLE_#{name.to_s.upcase}"]
+        value = instance_eval(&default) if value.nil?
+        value
+      end
+    end
+    private_class_method :env_flag
+
     (1..10).each {|v| define_method("bundler_#{v}_mode?") { major_version >= v } }
 
     settings_flag(:allow_bundler_dependency_conflicts) { bundler_2_mode? }
@@ -59,6 +68,8 @@ module Bundler
     settings_flag(:viz_command) { !bundler_2_mode? }
 
     settings_option(:default_cli_command) { bundler_2_mode? ? :cli_help : :install }
+
+    env_flag(:config_relative_to_cwd) { bundler_2_mode? }
 
     def initialize(bundler_version)
       @bundler_version = Gem::Version.create(bundler_version)

taximartin/rubygems

bundler/lib/bundler/feature_flag.rb
@@ -25,6 +25,15 @@ module Bundler
     end
     private_class_method :settings_method
 
+    def self.env_flag(name, &default)
+      define_method(:"#{name}?") do
+        value = ENV["BUNDLE_#{name.to_s.upcase}"]
+        value = instance_eval(&default) if value.nil?
+        value
+      end
+    end
+    private_class_method :env_flag
+
     (1..10).each {|v| define_method("bundler_#{v}_mode?") { major_version >= v } }
 
     settings_flag(:allow_bundler_dependency_conflicts) { bundler_3_mode? }
@@ -48,6 +57,8 @@ module Bundler
     settings_flag(:use_gem_version_promoter_for_major_updates) { bundler_3_mode? }
 
     settings_option(:default_cli_command) { bundler_3_mode? ? :cli_help : :install }
+
+    env_flag(:config_relative_to_cwd) { bundler_3_mode? }
 
     def initialize(bundler_version)
       @bundler_version = Gem::Version.create(bundler_version)